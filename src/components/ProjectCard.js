4e93a9c80c9cd1282980e7b402a398365aa0c224
25aw0102 <your_github_25aw0102@jec.ac.jp.com>
2026-03-05 05:17:09 +0900

    fix build

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,6 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl, url }) => {
+export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
@@ -8,10 +8,6 @@ export const ProjectCard = ({ title, description, imgUrl, url }) => {
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
-        {/* <br></br>
-          <div className="view-btn">
-              View Project
-            </div> */}
         </div>
       </div>
     </Col>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@ import { Container, Row, Col, Tab, Nav, Button } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/wetland.png";
 import projImg2 from "../assets/img/shrine.jpeg";
-import projImg3 from "../assets/img/AllenWalker.svg";
+import projImg3 from "../assets/img/allenwalker.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
